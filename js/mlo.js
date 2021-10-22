b3a981228f318b4c7da6891178b1ca5f9bec20e1
Yuli016 <ypantoja@csumb.edu>
2021-10-21 17:16:31 -0700

    Testing

---
+++ js/mlo.js
@@ -0,0 +1,8 @@
+
+jQuery(function(){
+
+        jQuery('.showSingle').click(function(){
+              jQuery('.targetDiv').hide();
+              jQuery('#div'+jQuery(this).attr('target')).show();
+        });
+});
